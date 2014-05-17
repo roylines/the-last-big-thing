5e7328c9e19c767358ed6e27b3b44f7f0b4c26a7
roylines <roy@roylines.co.uk>
2014-05-17 20:38:54 +0100

    better error handling

--- static/index.js
+++ static/index.js
@@ -1,15 +1,22 @@
 var success = function(data) {
   $('#lbt').html(data);
+  poll();
+};
+
+var error = function() {
+  setTimeout(poll, 10000);
+};
+
+var poll = function() {
+  $.ajax({
+    url: "data",
+    success: success,
+    dataType: "html",
+    error: error,
+    timeout: 30000
+  });
 };
 
 $(function() {
-  (function poll() {
-    $.ajax({
-      url: "data",
-      success: success,
-      dataType: "html",
-      complete: poll,
-      timeout: 30000
-    });
-  })();
+  poll();
 });
