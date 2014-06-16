a4439650032e734f4ed1b7c0702028aed07d8804
roylines <roy@roylines.co.uk>
2014-06-16 19:58:46 +0100

    another heroku attempt

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ var content = require('./lib/content'),
   router = require('router')(),
   server = require('http').Server(),
   io = require('socket.io')(server);
-//app = require('http').createServer(router),
-//io = require('socket.io')(app);
-
-server.on('request', router);
-
-// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
-console.log('transports', io.engine.transports);
-io.set('transports', io.engine.transports);
-if (process.env.HEROKU === 'true') {
-//  io.set("transports", ["xhr-polling"]);
-  //io.set("polling duration", 10);
-};
 
 var port = process.env.PORT || 8000;
 
-//app.listen(port);
+server.on('request', router);
 server.listen(port);
 
 console.log('Server running on ' + port);
@@ -46,5 +34,9 @@ router.post('/integration/{integration}/{token}', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
-  console.log('connected socket', socket);
+  if (process.env.HEROKU === 'true') {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+  }
+//  console.log('connected socket', socket);
 });
